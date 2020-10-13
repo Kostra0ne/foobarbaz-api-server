3b120691bd8240b44c47af52c2b5343afc0b2fc0
KostraOne <guillaume.amangoua@gmail.com>
2020-10-13 17:23:18 +0200

    added try/catch block on mongo connect

--- config/mongo.js
+++ config/mongo.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 
-mongoose.connection.on("connected", () =>
-  console.log("yay mongodb connected :)")
-);
+  mongoose.connection.on("connected", () =>
+    console.log("yay mongodb connected :)")
+  );
 
-mongoose.connection.on("error", () =>
-  console.log("nay db connection error :(")
-);
-
+  mongoose.connection.on("error", () =>
+    console.log("nay db connection error :(")
+  );
+} catch (dbErr) {
+  console.eeror(dbErr);
+}
